9c85d12505f6f63fc8066f70afcbe1b582dcad86
tanixu <627676436@qq.com>
2015-03-12 10:51:53 +0800

    创建项目

---
+++ sea-modules/main.js
@@ -0,0 +1,19 @@
+define(function(require, exports, module) {
+	var angular = require("angularjs");
+	var $ = require("jquery.min.js");
+	var main = angular.module("main",[]);
+	
+	main.controller("mainController",['$scope', '$http',
+		function($scope, $http) {
+//			$http.get('http://localhost:8080/hotelmanage/checkUser').success(function(data) {
+//				//alert(data);
+//			}).error(function(){
+//				//alert('error');
+//			});
+			$http.get("assets/json/index-nav.json").success(function(data){
+				$scope.navList = data;
+			})
+		}
+	])
+	module.exports = main;
+});
